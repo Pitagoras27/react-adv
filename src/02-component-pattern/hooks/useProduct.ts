3466d88d14317d42aa191a4c8d8d37d99080f6ec
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-05-07 22:00:29 -0500

    refac: Add alternative form to controlProps

--- src/02-component-pattern/hooks/useProduct.ts
+++ src/02-component-pattern/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { IOnChangeArgs, Product } from "../interfaces/interfaces";
 
 interface IUseProductArgs {
@@ -9,22 +9,16 @@ interface IUseProductArgs {
 
 export const useProduct = ({ onChange, product, value = 0 }: IUseProductArgs) => {
     const [count, setCount] = useState(0);
-    const IS_CONTROLLED = useRef(!!onChange);
 
     useEffect(() => {
         setCount(value)
     }, [value])
 
     const handleCount = (val: number) => {
-        
-        if(IS_CONTROLLED) {
-            onChange!({ count: val, product });
-            return;
-        }
-        // const NEW_COUNT = Math.max(count + val, 0);
+        const NEW_COUNT = Math.max(count + val, 0);
 
-        // setCount(NEW_COUNT)
-        // onChange && onChange({ count: NEW_COUNT, product });
+        setCount(NEW_COUNT)
+        onChange && onChange({ count: NEW_COUNT, product });
     }
 
     return {
